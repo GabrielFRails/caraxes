edbce2a17162e381adb80d90fa4089d834accaf4
GabrielFRails <gabriel.estudy.reis@gmail.com>
2025-05-08 11:01:22 -0300

    Create tokens.h

---
+++ lexer/tokens.h
@@ -0,0 +1,40 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+#define PROGRAMA       1
+#define CAR            2
+#define INT            3
+#define RETORNE        4
+#define LEIA           5
+#define ESCREVA        6
+#define NOVALINHA      7
+#define SE             8
+#define ENTAO          9
+#define SENAO         10
+#define ENQUANTO      11
+#define EXECUTE       12
+#define ID            13
+#define INTCONST      14
+#define CARCONST      15
+#define STRING        16
+#define PLUS          17
+#define MINUS         18
+#define TIMES         19
+#define DIV           20
+#define ASSIGN        21
+#define EQ            22
+#define NEQ           23
+#define LT            24
+#define GT            25
+#define LE            26
+#define GE            27
+#define LPAREN        28
+#define RPAREN        29
+#define LBRACE        30
+#define RBRACE        31
+#define SEMI          32
+#define COMMA         33
+#define OR            34
+#define AND           35
+
+#endif
